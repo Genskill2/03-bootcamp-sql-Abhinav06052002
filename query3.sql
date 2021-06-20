43ba07b98c92511f981722f63adde31bb3935ab2
Abhinav <jammisaiabhinav@gmail.com>
2021-06-20 19:18:50 +0530

    Adds solution files

--- query3.sql
+++ query3.sql
@@ -1,1 +1,1 @@
-select title from books b, subjects s, books_subjects bs where bs.book = b.id and bs.subject = s.id and s.name = 'Politics' or 'Technology';
+select title from books b, subjects s, books_subjects bs where bs.book = b.id and bs.subject = s.id and s.name = 'Technology' or 'Politics';
